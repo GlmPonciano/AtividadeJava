cc4fea1bd861eb9566528623e94bbeff77e93574
GlmPonciano <guilherme.miranda251114@gmail.com>
2025-04-03 15:08:11 -0300

    Criado aplicações em java

---
+++ while/Att1.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Att1 {
+    public static void main(String[] args) {
+        System.out.println("Números ímpares de 1 a 100:");
+
+        for (int i = 1; i <= 100; i++) {
+        if (i % 2 != 0) {
+        System.out.println(i);
+            }
+        }
+    }
+}
---
+++ while/Att8.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Att8 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int l = 0;
+        System.out.println("Digite um numero entre 1 e 20");
+        do{
+            System.out.print("Escreva o tamanho do lado:");
+            l = scan.nextInt();
+            
+        }while(l>20 || l<1);
+        System.out.println("");
+        for(int i=0;i<l;i++){
+            for(int j=0;j<l;j++){
+                if(i == 0 || i==l-1){
+                    System.out.print("*");
+                }else{
+                    if(j==0 || j==l-1){
+                        System.out.print("*");
+                    }else{
+                        System.out.print(" ");
+                    }
+                    
+                }
+            }
+            System.out.println("");
+        }
+
+        scan.close();
+    }
+}
